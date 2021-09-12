c1b3c14182cfd111f73f7dbb7d921ac4b5c9107c
jatwell93 <atwellj93@gmail.com>
2021-09-12 16:26:56 +1000

    Starting part 4

--- Project 1/Assets/Scripts/PlayerController.cs
+++ Project 1/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    private float turnSpeed;
+    public float horizontalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +18,11 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //lower case t refers to a component of the current object (playercontroller). Upper case T is a seperate class
+        //Input gets us the input manager then getaxis gets us axis referred to in the brackets
+        horizontalInput = Input.GetAxis("Horizontal");
+
         //transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
